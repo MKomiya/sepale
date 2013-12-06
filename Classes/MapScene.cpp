ea4c55a87e27fda74fcea3d420b9231e54480618
Mitsushige Komiya <s.wyvern.acespring@gmail.com>
2013-12-06 12:33:13 +0900

    Add view Player character

--- Classes/MapScene.cpp
+++ Classes/MapScene.cpp
@@ -24,6 +24,32 @@ bool MapScene::init()
     {
         return false;
     }
-     
+    
+    _viewPlayerCharacter();
+    
     return true;
+}
+
+void MapScene::_viewPlayerCharacter()
+{
+    // キャラチップの読み込みとエイジング無効化
+    CCTexture2D* texture = CCTextureCache::sharedTextureCache()->addImage("charachip/hiroine_1616.png");
+    texture->setAliasTexParameters();
+    
+    // フレーム分割
+    CCSpriteFrameCache* frameCache = CCSpriteFrameCache::sharedSpriteFrameCache();
+    frameCache->addSpriteFramesWithFile("charachip/hiroine.plist", "charachip/hiroine_1616.png");
+    
+    // アニメーションの設定
+    CCSprite* sprite = CCSprite::create();
+    CCAnimationCache* cache = CCAnimationCache::sharedAnimationCache();
+    cache->addAnimationsWithFile("charachip/animations.plist");
+    CCAnimation* animation = cache->animationByName("down_anime");
+    CCAnimate* action = CCAnimate::create(animation);
+    action->setTag(kPlayerAnimateTags);
+    sprite->runAction(action);
+    
+    CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+    sprite->setPosition(ccp(winSize.width/2, winSize.height/2 + 40));
+    this->addChild(sprite);
 }
--- Classes/MapScene.h
+++ Classes/MapScene.h
@@ -14,9 +14,17 @@
 class MapScene : public cocos2d::CCLayer
 {
 public:
+    enum Tags
+    {
+        kPlayerAnimateTags = 1
+    };
     virtual bool init();
     static cocos2d::CCScene* scene();
     CREATE_FUNC(MapScene);
+    
+private:
+    // ユーザが移動させるプレイヤーの表示
+    void _viewPlayerCharacter();
 };
 
 #endif /* defined(__sepale__MapScene__) */
